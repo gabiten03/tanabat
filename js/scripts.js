719092b1c76b3b6cdffa2e5dbeeac33d630b5185
TI-Lorca <gabriel.lorenzo@bue.edu.ar>
2023-02-06 09:52:44 -0300

    00

--- js/scripts.js
+++ js/scripts.js
@@ -20,33 +20,7 @@ $(document).ready(function() {
 
 
 
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=IGQVJWcHNmTE8yME1wUVo2SUhwZA0NIdDVGdEJ6UXNaaGhvc0JtTDJQNW1SYkprWjhzOTlBbDJTVFphZADh4NnNIVzJwQXM1eU5RSlRra2RVVlNvbDBqMXZAiQW9XZAy1BWWRWcld1XzdYdG1FV3ZAGUk8yZAAZDZD&count=9', true);
-
-    request.onload = function(container) {
-      if (request.status >= 200 && request.status < 400) {
-        // Success!
-        var data = JSON.parse(request.responseText);
-        for (var i=0;i < data.data.length;i++) {
-            var container = document.getElementById('insta-feed');
-            var imgURL = data.data[i].images.standard_resolution.url;
-            console.log(imgURL);
-            var div = document.createElement('div');
-            div.setAttribute('class','instapic');
-            container.appendChild(div);
-            var img = document.createElement('img');
-            img.setAttribute('src',imgURL)
-            div.appendChild(img);
-        }
-
-        console.log(data);
-      } else {
-      }
-    };
-    request.onerror = function() {
-      // There was a connection error of some sort
-    };
-    request.send();
+    
 
 
 
